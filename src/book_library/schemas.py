2e452d3aab2016645b79c93f81602c9334302b05
arad <aradarvinxo@gmail.com>
2023-10-20 16:57:31 +0330

    Chore: schema for update books

--- src/book_library/schemas.py
+++ src/book_library/schemas.py
@@ -1,3 +1,8 @@
+import uuid
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
 
 class Book(BaseModel):
     id: str = Field(default_factory=uuid.uuid4, alias="_id")
@@ -9,3 +14,9 @@ class Book(BaseModel):
         allow_population_by_field_name = True
 
 
+
+class UpdateBook(BaseModel):
+    id: str = Field(default_factory=uuid.uuid4, alias="_id")
+    title: str = Field()
+    pages: int = Field()
+    author: str = Field()
