55125f3192b2e9395b6183a9979bf2092b43b60b
arad <aradarvinxo@gmail.com>
2023-10-20 17:30:37 +0330

    Chore: publisher field and optional for update

--- src/book_library/schemas.py
+++ src/book_library/schemas.py
@@ -9,6 +9,7 @@ class Book(BaseModel):
     title: str = Field()
     pages: int = Field()
     author: str = Field()
+    publisher: str = Field()
 
     class Config:
         populate_by_name = True
@@ -17,6 +18,7 @@ class Book(BaseModel):
 
 class UpdateBook(BaseModel):
     _id: str
-    title: str = Field()
-    pages: int = Field()
-    author: str = Field()
+    title: str = Optional[str]
+    pages: int = Optional[int]
+    author: str = Optional[str]
+    publisher: str = Optional[str]
