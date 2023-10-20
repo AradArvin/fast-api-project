a36ba69289fba50b90f98c79eadd1f4d08e209f1
arad <aradarvinxo@gmail.com>
2023-10-20 17:21:29 +0330

    Fix: list books id is str now

--- src/book_library/routes.py
+++ src/book_library/routes.py
@@ -1,15 +1,16 @@
 from fastapi import APIRouter, Body, Request, Response, HTTPException, status
 from fastapi.encoders import jsonable_encoder
 from .schemas import Book
-from typing import List
 from .db import collection
+from bson.objectid import ObjectId
 
 router = APIRouter()
 
 
-@router.post("/", response_description="Add a new book", status_code=status.HTTP_201_CREATED, response_model=Book)
-async def create_books(request: Request, book: Book = Body()):
+@router.post("/", response_description="Add a new book", status_code=status.HTTP_201_CREATED)
+async def create_books(book: Book = Body()):
     book = jsonable_encoder(book)
+
     new_book = collection.insert_one(book)
     created_book = collection.find_one(
         {"_id": new_book.inserted_id}
@@ -18,7 +19,14 @@ async def create_books(request: Request, book: Book = Body()):
     return created_book
 
 
-@router.get("/", response_description="List all books", status_code=status.HTTP_200_OK, response_model=List[Book])
-def list_books(request: Request):
-    books = list(collection.find())
-    return books
+@router.get("/", response_description="List all books", status_code=status.HTTP_200_OK)
+def list_books():
+    book_list = list()
+    
+    books = collection.find()
+    
+    for book in books:
+        book["_id"] = str(book["_id"])
+        book_list.append(book)
+
+    return book_list
