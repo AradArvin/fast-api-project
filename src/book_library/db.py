2517021d6ee68ad03caf96e8f4f4d5ec503209f7
arad <aradarvinxo@gmail.com>
2023-10-21 17:06:28 +0330

    Chore: init method for mongodb

--- src/book_library/db.py
+++ src/book_library/db.py
@@ -9,3 +9,9 @@ class MongoDBConnectionManager:
     HOST_ADDRESS = "mongodb://localhost:27017/"
 
 
+    def __init__(self, database: str, collection: str) -> None:
+        self.client = MongoClient(self.HOST_ADDRESS)
+        self.database = self.client[database]
+        self.collection = self.database[collection]
+    
+
