805748e63c60c513f94f938c826c5af8c92389f0
arad <aradarvinxo@gmail.com>
2023-10-21 17:06:03 +0330

    Feat: manager class for mongodb connection

--- src/book_library/db.py
+++ src/book_library/db.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
-# from pydantic import BaseModel
+from pydantic import BaseModel
+from bson.objectid import ObjectId
 
 
 
-client = MongoClient("mongodb://localhost:27017/")
-db = client.book_library
-collection = db.q_search
+class MongoDBConnectionManager:
+
+    HOST_ADDRESS = "mongodb://localhost:27017/"
+
 
