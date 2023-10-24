685ca8951d0271d4ab8112df5e0948fe2d2048d7
arad <aradarvinxo@gmail.com>
2023-10-24 20:42:16 +0330

    Chore: router for users included

--- src/main.py
+++ src/main.py
@@ -1,8 +1,10 @@
 from fastapi import FastAPI
 from book_library.routes import router as book_router
+from jwt_auth.routes import router as user_router
 
 
 app = FastAPI()
 
 app.include_router(book_router, tags=["books"], prefix="/book")
+app.include_router(user_router, tags=["users"], prefix="/user")
 
