b04c8a7b28c19707151e19e08646896204f5fb7f
arad <aradarvinxo@gmail.com>
2023-10-25 22:35:35 +0330

    Refactor: new conditions and exceptions

--- src/jwt_auth/routes.py
+++ src/jwt_auth/routes.py
@@ -63,18 +63,22 @@ def check_token(data: UserLogin):
 
 @router.post("/login", response_description="Login to you'r user account using email & password", status_code=status.HTTP_200_OK)
 async def user_login(user: UserLogin = Body()):
+    user = jsonable_encoder(user)
     if check_user(user):
-        access_token = access_token_gen(user._id)
-        refresh_token = refresh_token_gen(user._id)
+        if check_token(user):
+            user_data = user_collection.find_data_by_another_field("email", user["email"])
+            access_token = access_token_gen(user_data["_id"])
+            refresh_token = refresh_token_gen(user_data["_id"])
 
-        refresh_dict = {
-            "user_id": str(user._id),
-            "refresh_token": refresh_token,
-            }
-        json_encoded_refresh = jsonable_encoder(refresh_dict)
-        jwt_collection.save_data_to_db_collection(instance=json_encoded_refresh)
+            refresh_dict = {
+                "user_id": str(user_data["_id"]),
+                "refresh_token": refresh_token,
+                }
+            json_encoded_refresh = jsonable_encoder(refresh_dict)
+            jwt_collection.save_data_to_db_collection(instance=json_encoded_refresh)
 
-        return token_response(access_token, "access")
-    return {
-        "error": "Wrong login Data!",
-    }
+            return token_response(access_token, "access")
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="User is already logged in!")
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found. Please check login data!")
+
+
