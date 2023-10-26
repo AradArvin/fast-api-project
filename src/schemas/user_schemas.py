33d975d205ea6f8a1e3978bf72412818a124a819
arad <aradarvinxo@gmail.com>
2023-10-26 09:50:18 +0330

    Chore: user schemas in new schemas package

---
+++ src/schemas/user_schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel, Field, EmailStr
+
+
+
+class User(BaseModel):
+    _id: str
+    username: str = Field()
+    email: EmailStr = Field()
+    password: str = Field()
+
+    class Config:
+        populate_by_name = True
+
+
+class UserLogin(BaseModel):
+    _id: str
+    email: EmailStr = Field()
+    password: str = Field()
