119008a6f1e2c704765092ae8e01730fae5f0f82
arad <aradarvinxo@gmail.com>
2023-10-26 09:50:08 +0330

    Chore: book schemas in new schemas package

---
+++ src/schemas/book_schemas.py
@@ -0,0 +1,23 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+
+class Book(BaseModel): 
+    _id: str
+    title: str = Field()
+    pages: int = Field()
+    author: str = Field()
+    publisher: str = Field()
+
+    class Config:
+        populate_by_name = True
+
+
+
+class UpdateBook(BaseModel):
+    _id: str
+    title: str = Optional[str]
+    pages: int = Optional[int]
+    author: str = Optional[str]
+    publisher: str = Optional[str]
